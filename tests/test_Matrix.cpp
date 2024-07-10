e4f8edb300defdeacc5ebb8ccde80aa4abb0443d
PanChrzan <maciej.chrzanowski98@gmail.com>
2024-07-10 20:28:32 +0200

    Refactor matrix addition test.

--- tests/test_Matrix.cpp
+++ tests/test_Matrix.cpp
@@ -47,24 +47,25 @@ TEST(Matrix__Test, ExpectWrongDimensionsThrow)
 TEST(Matrix__Test, AddingTwoMatrices)
 {
     // GIVEN
-    Matrix<int> mat_left(2, 2), mat_right(2, 2), expected(2, 2);
-    mat_left(1, 1) = 1;
-    mat_left(1, 2) = 1;
-    mat_left(2, 1) = 1;
-    mat_left(2, 2) = 1;
-    mat_right(1, 1) = 2;
-    mat_right(1, 2) = 2;
-    mat_right(2, 1) = 2;
-    mat_right(2, 2) = 2;
-    expected(1, 1) = 3;
-    expected(1, 2) = 3;
-    expected(2, 1) = 3;
-    expected(2, 2) = 3;
+    const int SIZE = 2;
+    Matrix<int> mat_left(SIZE, SIZE), mat_right(SIZE, SIZE), expected(SIZE, SIZE);
+    for(unsigned int i=1; i<=SIZE; i++)
+    {
+        for(unsigned int j=1; j<=SIZE; j++)
+        {
+            mat_left(i, j) = 1;
+            mat_right(i, j) = 2;
+            expected(i, j) = 3;
+        }
+    }
     //WHEN
     Matrix<int> ret = mat_left + mat_right;
     //THEN
-    ASSERT_EQ(expected(1, 1), ret(1, 1));
-    ASSERT_EQ(expected(1, 2), ret(1, 2));
-    ASSERT_EQ(expected(2, 1), ret(2, 1));
-    ASSERT_EQ(expected(2, 2), ret(2, 2));
+    for(unsigned int i=1; i<=SIZE; i++)
+    {
+        for(unsigned int j=1; j<=SIZE; j++)
+        {
+            ASSERT_EQ(expected(i, j), ret(i, j));
+        }
+    }
 }
